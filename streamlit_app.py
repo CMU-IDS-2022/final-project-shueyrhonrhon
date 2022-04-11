f92124e12b51ff7f3281189f59b5f074f25b58e3
wujiaxiang <jiaxianw@andrew.cmu.edu>
2022-04-11 16:09:43 -0400

    add two bar charts and load data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,59 @@
+from sklearn.preprocessing import scale
 import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
 
 st.title("Data Scientist Career Accelerator")
+@st.cache
 def load_data():
     """
     Write 1-2 lines of code here to load the data from CSV to a pandas dataframe
     and return it.
     """
-    pass
+    data_path = "ds_data.csv"
+    df = pd.read_csv(data_path)
+    return df
+
+@st.cache
+def get_slice(df,state):
+    lable = pd.Series([True]*len(df),index=df.index)
+    if state:
+        lable &= df['Job Location'].apply(lambda x : True if x==state else False)
+    return lable
+
+
+df = load_data()
+if st.checkbox("Show raw data"):
+    st.write(df[:25])
+
+st.header("The salary map of Data Scientist in the US")
+
+
+national_salary_chart = alt.Chart(df).mark_bar(color='#FF8080').encode(
+    x = alt.X("avgSalary", bin=alt.Bin(extent=[0,300],step=50)),
+    y=alt.Y("count()"),
+    tooltip = ['avgSalary', 'count()']
+)
+
+stateOption = "CA"
+slices = get_slice(df,stateOption)
+st.header("The salary in "+stateOption+" and the US")
+state_salary_chart = alt.Chart(df[slices]).mark_bar(color="#C0EDA6").encode(
+    x=alt.X("avgSalary", bin=alt.Bin(extent=[0,300],step=50)),
+    y=alt.Y("count()"),
+    tooltip = ['avgSalary', 'count()']
+)
+
+st.header("Top industry average salary in "+stateOption)
+top_industry_salary_chart = alt.Chart(df[slices]).mark_bar(color="#4D77FF").encode(
+    x=alt.X("Industry",sort="-y"),
+    y=alt.Y("mean(avgSalary)"),
+    tooltip = ['Industry', 'mean(avgSalary)']
+)
+
+st.write(national_salary_chart+state_salary_chart)
+st.write(top_industry_salary_chart)
 
 st.header("Annual Salary Prediction")
 
