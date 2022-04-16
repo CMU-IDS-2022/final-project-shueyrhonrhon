b4bd5ab0b38167eef7e357e4c9bbfdae2c85bdd3
litanzhiyi <zhiyili@andrew.cmu.edu>
2022-04-16 01:17:58 -0400

    add model prediction, poor results

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,13 @@ import pandas as pd
 import altair as alt
 import numpy as np
 
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+# from matplotlib import pyplot
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.svm import SVR
+
 st.title("Data Scientist Career Accelerator")
 @st.cache
 def load_data():
@@ -54,6 +61,57 @@ top_industry_salary_chart = alt.Chart(df[slices]).mark_bar(color="#4D77FF").enco
 
 st.write(national_salary_chart+state_salary_chart)
 st.write(top_industry_salary_chart)
+
+def Xgb_Regression():
+    # model = xgb.XGBRegressor()
+    model = xgb.XGBRegressor(max_depth=7, eta=0.1,n_estimators=1000, learning_rate=0.1, use_label_encoder=False)
+    return model
+def Svm_regression():
+    model = SVR(kernel='rbf')
+    return model
+
+
+def train_model(x,y):
+    #model = Xgb_Regression()
+    model = Svm_regression()
+    print(x.shape)
+    print(y.shape)
+    # xtrain, xtest, ytrain, ytest=train_test_split(x, y, test_size=0.2)
+    xtrain = x
+    ytrain = y
+    model.fit(xtrain, ytrain)
+    # feature importance
+    # print(model.feature_importances_)
+    # #print(model.feature_importances_.T.shape)
+    # # plot
+    # # pyplot.bar(range(len(model.feature_importances_)), model.feature_importances_)
+    # # pyplot.show()
+    # df = pd.DataFrame(model.feature_importances_,columns=['importance'])
+    # df['skills'] = x.columns
+    # #df.reindex(x.columns, axis=1)
+    # print(df)
+    # feature_chart = alt.Chart(df).mark_bar(color="#C0EDA6").encode(
+    #   x=alt.X('skills',sort="-y"),
+    #   y=alt.Y('importance'),
+    # )
+    # st.write(feature_chart)
+    #   x=alt.X("Industry",sort="-y"),
+    # y=alt.Y("mean(avgSalary)"),
+    # tooltip = ['Industry', 'mean(avgSalary)']
+
+    #evaluate
+    scores = cross_val_score(model, xtrain, ytrain,cv=10)
+    print("Mean cross-validation score: %.2f" % scores.mean())
+    # ypred = model.predict(xtest)
+    # mse = mean_squared_error(ytest, ypred)
+    # print("MSE: %.2f" % mse)
+
+
+Y = df.iloc[:,19]
+#print(Y.head)
+X = df.iloc[:,23:39]
+#print(X.head)
+train_model(X,Y)
 
 st.header("Annual Salary Prediction")
 
